2b48916788b5dcb2e0f20767197a07bdd7bd393d
htsnul <htsnul@gmail.com>
2024-02-17 10:48:02 +0900

    Introduce sequence concept

--- binop.js
+++ binop.js
@@ -19,31 +19,23 @@ function tokenize(str) {
   return tokens;
 }
 
-function parseTerms(tokens, i) {
-  const result = parseTerm(tokens, i);
-  if (!result) return;
-  i = result.i;
-  let expr = result.expr;
+function parseSequence(tokens, i) {
+  const exprs = [];
   while (i < tokens.length) {
-    const resultC = parseTerm(tokens, i);
-    if (!resultC) break;
-    i = resultC.i;
-    const resultR = parseTerm(tokens, i);
-    if (resultR) i = resultR.i;
-    const exprL = expr;
-    const exprC = resultC.expr;
-    const exprR = resultR?.expr ?? { type: "Null" };
-    expr = { type: "Call", func: exprC, args: { l: exprL, r: exprR } };
+    const result = parseExpr(tokens, i);
+    if (!result) break;
+    i = result.i;
+    exprs.push(result.expr);
   }
-  return { i, expr };
+  return { i, expr: { type: "Sequence", exprs } };
 }
 
-function parseTerm(tokens, i) {
+function parseExpr(tokens, i) {
   if (i >= tokens.length) return;
   const token = tokens[i];
   if (token.type === "GroupStart") {
     i++;
-    const result = parseTerms(tokens, i);
+    const result = parseSequence(tokens, i);
     if (result) i = result.i;
     const expr = result?.expr ?? { type: "Null" };
     console.assert(tokens[i].type === "GroupEnd");
@@ -61,29 +53,35 @@ function parseTerm(tokens, i) {
 }
 
 function parse(tokens) {
-  return parseTerms(tokens, 0)?.expr ?? { type: "Null" };
+  return parseSequence(tokens, 0)?.expr ?? { type: "Null" };
 }
 
-function evalCallExpr(expr) {
-  const argsLVal = evalExpr(expr.args.l);
-  const func = (() => {
-    const f = evalExpr(expr.func);
-    if (typeof f === "string") return envVal(f);
-    if (typeof f === "object") return f;
-  })();
+function applyFunc(func, l, rExpr) {
+  if (typeof func === "string") func = envVal(func);
   if (typeof func === "function") {
-    return func(argsLVal, expr.args.r);
+    return func(l, rExpr);
   }
   if (typeof func === "object") {
-    envStack.push({ args: { l: argsLVal, r: evalExpr(expr.args.r) } });
-    const result = evalExpr(func);
+    envStack.push({ args: { l, r: evalExpr(rExpr) } });
+    const val = evalExpr(func);
     envStack.pop();
-    return result;
+    return val;
   }
+}
+
+function evalSequenceExpr(expr) {
+  if (expr.exprs.length === 0) return undefined;
+  let val = evalExpr(expr.exprs[0]);
+  for (let i = 1; i < expr.exprs.length;) {
+    const func = evalExpr(expr.exprs[i++]);
+    const rExpr = expr.exprs[i++] ?? { type: "Null" };
+    val = applyFunc(func, val, rExpr);
+  }
+  return val;
 }
 
 function evalExpr(expr) {
-  if (expr.type === "Call") return evalCallExpr(expr);
+  if (expr.type === "Sequence") return evalSequenceExpr(expr);
   if (expr.type === "Number") return expr.value;
   if (expr.type === "String") return expr.value;
   if (expr.type === "Null") return undefined;
